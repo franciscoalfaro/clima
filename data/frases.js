933b6f69561e1adb86122347ec41216df1614e6c
franciscoalfar@gmail.com <*Fran2811*>
2024-05-04 21:43:16 -0400

    se agrega frase del dia

---
+++ data/frases.js
@@ -0,0 +1,109 @@
+
+export const dias = [
+  {
+    "id": 1,
+    "titulo": "Lunes",
+    "frase": "Hoy es el comienzo de una nueva semana, ¡aprovecha cada momento al máximo!"
+  },
+  {
+    "id": 2,
+    "titulo": "Martes",
+    "frase": "Recuerda que cada pequeño paso te acerca más a tus grandes metas."
+  },
+  {
+    "id": 3,
+    "titulo": "Miércoles",
+    "frase": "Enfrenta los retos con valentía y determinación, ¡puedes superarlos!"
+  },
+  {
+    "id": 4,
+    "titulo": "Jueves",
+    "frase": "Cree en ti mismo y en tu capacidad para alcanzar tus sueños."
+  },
+  {
+    "id": 5,
+    "titulo": "Viernes",
+    "frase": "¡Celebra tus logros de la semana y prepárate para un merecido descanso!"
+  },
+  {
+    "id": 6,
+    "titulo": "Sábado",
+    "frase": "Disfruta del día y dedica tiempo a hacer lo que amas."
+  },
+  {
+    "id": 7,
+    "titulo": "Domingo",
+    "frase": "Recarga energías para una semana llena de oportunidades y aprendizajes."
+  },
+  {
+    "id": 8,
+    "titulo": "Lunes",
+    "frase": "Nuevas semanas significan nuevas oportunidades. ¡Aprovecha al máximo cada día!"
+  },
+  {
+    "id": 9,
+    "titulo": "Martes",
+    "frase": "Tu determinación es tu mayor fortaleza. ¡Sigue adelante con confianza!"
+  },
+  {
+    "id": 10,
+    "titulo": "Miércoles",
+    "frase": "Cada obstáculo es una oportunidad para crecer y aprender."
+  },
+  {
+    "id": 11,
+    "titulo": "Jueves",
+    "frase": "Mantén tu enfoque en tus metas y no te detengas hasta alcanzarlas."
+  },
+  {
+    "id": 12,
+    "titulo": "Viernes",
+    "frase": "El fin de semana está cerca, pero aún puedes lograr grandes cosas hoy."
+  },
+  {
+    "id": 13,
+    "titulo": "Sábado",
+    "frase": "Haz hoy lo que te acerca a donde quieres estar mañana."
+  },
+  {
+    "id": 14,
+    "titulo": "Domingo",
+    "frase": "Aprovecha este día para descansar, reflexionar y prepararte para la semana que viene."
+  },
+  {
+    "id": 15,
+    "titulo": "Lunes",
+    "frase": "Cada nueva semana trae consigo nuevas posibilidades. ¡Haz que cuente!"
+  },
+  {
+    "id": 16,
+    "titulo": "Martes",
+    "frase": "Recuerda que cada pequeño progreso te acerca un paso más a tus metas."
+  },
+  {
+    "id": 17,
+    "titulo": "Miércoles",
+    "frase": "Con cada desafío superado, te vuelves más fuerte y más sabio."
+  },
+  {
+    "id": 18,
+    "titulo": "Jueves",
+    "frase": "Confía en tu capacidad para superar cualquier obstáculo que se interponga en tu camino."
+  },
+  {
+    "id": 19,
+    "titulo": "Viernes",
+    "frase": "Celebra tus logros de la semana y prepárate para descansar y recargar energías."
+  },
+  {
+    "id": 20,
+    "titulo": "Sábado",
+    "frase": "Hoy es el día perfecto para hacer lo que amas y recargar tu espíritu."
+  },
+  {
+    "id": 21,
+    "titulo": "Domingo",
+    "frase": "Aprovecha este día para relajarte, reflexionar y prepararte para una nueva semana llena de oportunidades."
+  }
+]
+
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Spinner from './Spinner';
+import { dias } from '../../data/frases';
 
 
 export const Tarjeta = ({ infoData, loadingData, clima, proximashoras }) => {
+
+    const [frase, setFrase] = useState({});
+
+    useEffect(() => {
+        // Obtenemos el día actual como un nombre de día de la semana
+        const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        const diaActualNombre = diasSemana[new Date().getDay()];
+
+        // Filtramos las frases que coinciden con el día actual por su título
+        const frasesCoincidentes = dias.filter(dia => dia.titulo === diaActualNombre);
+
+        // Si hay más de una frase coincidente, seleccionamos una al azar
+        if (frasesCoincidentes.length > 0) {
+            const fraseAleatoria = frasesCoincidentes[Math.floor(Math.random() * frasesCoincidentes.length)];
+            setFrase(fraseAleatoria);
+        } else if (frasesCoincidentes.length === 1) { // Si hay exactamente una frase coincidente, la seleccionamos directamente
+            setFrase(frasesCoincidentes[0]);
+        }
+    }, []);
+
+
     var today = new Date();
     var day = today.getDate();
     var month = today.getMonth() + 1;
@@ -47,50 +69,58 @@ export const Tarjeta = ({ infoData, loadingData, clima, proximashoras }) => {
                 infoData === true ? (
 
                     <div id="weatherResults" className="grid grid-cols-1 md:grid-cols-2 gap-4 text-white">
-                            
-                                <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md">
-                                    <h2 className="text-xl font-semibold mb-2">{clima.name}</h2>
-                                    <p className="text-gray-300">Fecha: {date}</p>
-                                    <p className="text-gray-300">Temperatura: {(clima.main.temp - 273.15).toFixed(1)}ºC</p>
-                                    <p className="text-gray-300">Descripción: {clima.weather[0].description} <img src={iconUrl} className="inline-block ml-2"></img></p>
-                                </div>
-                                <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md">
-                                    <h5 className="text-gray-300">Temperatura máxima: {(clima.main.temp_max - 273.15).toFixed(1)}ºC</h5>
-                                    <h5 className="text-gray-300">Temperatura mínima: {(clima.main.temp_min - 273.15).toFixed(1)}ºC</h5>
-                                    <h5 className="text-gray-300">Sensación térmica: {(clima.main.feels_like - 273.15).toFixed(1)}ºC</h5>
-                                    <h5 className="text-gray-300">Humedad: {clima.main.humidity}%</h5>
-                                    <h5 className="text-gray-300">Velocidad del viento: {clima.wind.speed}m/s</h5> <img src={iconUrlWind} className="inline-block ml-2"></img>
-                                </div>
-                          
 
-                            <div className="grid grid-cols-1 gap-4">
-                                <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
-                                    <div className="col">
-                                        <p>{proximashorasDia3}h</p>
-                                        <p className="text-gray-300">Descripción: {proximashoras.list[1].weather[0].description} <img src={iconUrl3} className="inline-block ml-2"></img></p>
-                                        <p className="text-gray-300">Temperatura: {(proximashoras.list[1].main.temp - 273.15).toFixed(1)}ºC</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="grid grid-cols-1 gap-4">
-                                <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
-                                    <div className="col">
-                                        <p>{proximashorasDia6}h</p>
-                                        <p className="text-gray-300">Descripción: {proximashoras.list[2].weather[0].description} <img src={iconUrl6} className="inline-block ml-2"></img></p>
-                                        <p className="text-gray-300">Temperatura: {(proximashoras.list[2].main.temp - 273.15).toFixed(1)}ºC</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="grid grid-cols-1 gap-4">
-                                <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
-                                    <div className="col">
-                                        <p>{proximashorasDia9}h</p>
-                                        <p className="text-gray-300">Descripción: {proximashoras.list[3].weather[0].description} <img src={iconUrl9} className="inline-block ml-2"></img></p>
-                                        <p className="text-gray-300">Temperatura: {(proximashoras.list[3].main.temp - 273.15).toFixed(1)}ºC</p>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md">
+                            <h2 className="text-xl font-semibold mb-2">{clima.name}</h2>
+                            <p className="text-gray-300">Fecha: {date}</p>
+                            <p className="text-gray-300">Temperatura: {(clima.main.temp - 273.15).toFixed(1)}ºC</p>
+                            <p className="text-gray-300">Descripción: {clima.weather[0].description} <img src={iconUrl} className="inline-block ml-2"></img></p>
+                        </div>
+                        <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md">
+                            <h5 className="text-gray-300">Temperatura máxima: {(clima.main.temp_max - 273.15).toFixed(1)}ºC</h5>
+                            <h5 className="text-gray-300">Temperatura mínima: {(clima.main.temp_min - 273.15).toFixed(1)}ºC</h5>
+                            <h5 className="text-gray-300">Sensación térmica: {(clima.main.feels_like - 273.15).toFixed(1)}ºC</h5>
+                            <h5 className="text-gray-300">Humedad: {clima.main.humidity}%</h5>
+                            <h5 className="text-gray-300">Velocidad del viento: {clima.wind.speed}m/s</h5> <img src={iconUrlWind} className="inline-block ml-2"></img>
+                        </div>
 
+
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
+                                <div className="col">
+                                    <p>{proximashorasDia3}h</p>
+                                    <p className="text-gray-300">Descripción: {proximashoras.list[1].weather[0].description} <img src={iconUrl3} className="inline-block ml-2"></img></p>
+                                    <p className="text-gray-300">Temperatura: {(proximashoras.list[1].main.temp - 273.15).toFixed(1)}ºC</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
+                                <div className="col">
+                                    <p>{proximashorasDia6}h</p>
+                                    <p className="text-gray-300">Descripción: {proximashoras.list[2].weather[0].description} <img src={iconUrl6} className="inline-block ml-2"></img></p>
+                                    <p className="text-gray-300">Temperatura: {(proximashoras.list[2].main.temp - 273.15).toFixed(1)}ºC</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
+                                <div className="col">
+                                    <p>{proximashorasDia9}h</p>
+                                    <p className="text-gray-300">Descripción: {proximashoras.list[3].weather[0].description} <img src={iconUrl9} className="inline-block ml-2"></img></p>
+                                    <p className="text-gray-300">Temperatura: {(proximashoras.list[3].main.temp - 273.15).toFixed(1)}ºC</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="bg-black bg-opacity-50 p-4 rounded-3xl shadow-md flex items-center justify-between">
+                                <div className="col">
+                                    <h2>Frase del dia {frase.titulo}</h2>
+                                    <p>{frase.frase}</p>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
