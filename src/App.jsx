d7d9ae169ee878c03976638ca755964bff376f23
Francisco alfaro <franciscoalfar@gmail.com>
2024-05-08 01:10:32 -0400

    mejoras en el boton

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,15 @@ function App() {
 
   return (
     <>
-      <div className="py-8 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        <button onClick={handleChangeTheme} className="dark:bg-gray-800 dark:text-white rounded-full py-2 px-4 bg-gray-200 text-gray-800 rounded-full shadow-md">
+      <div className="relative">
+        <button onClick={handleChangeTheme} className="fixed bottom-0 right-0 m-8 dark:bg-gray-800 dark:text-white rounded-full py-2 px-4 bg-gray-200 text-gray-800 rounded-full shadow-md z-50">
           Modo oscuro
         </button>
-        <div id="weatherContainer" className="text-overlay p-8 rounded-lg">
-          <h1 className="text-3xl font-bold mb-4 text-white">Clima de tu ciudad</h1>
-          <Panel></Panel>
+        <div className="py-8 dark:bg-slate-900/60 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+          <div id="weatherContainer" className="text-overlay p-8 rounded-lg">
+            <h1 className="text-3xl font-bold mb-4 text-white">Clima de tu ciudad</h1>
+            <Panel></Panel>
+          </div>
         </div>
       </div>
 
