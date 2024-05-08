26e225e5f80d7d7a8ce610ea6f79b311e15e4152
Francisco alfaro <franciscoalfar@gmail.com>
2024-05-08 01:20:31 -0400

    black background

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <button onClick={handleChangeTheme} className="fixed bottom-0 right-0 m-8 dark:bg-gray-800 dark:text-white rounded-full py-2 px-4 bg-gray-200 text-gray-800 rounded-full shadow-md z-50">
           Modo oscuro
         </button>
-        <div className="py-8 dark:bg-slate-900/60 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <div className="py-8 dark:bg-slate-800/90 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
           <div id="weatherContainer" className="text-overlay p-8 rounded-lg">
             <h1 className="text-3xl font-bold mb-4 text-white">Clima de tu ciudad</h1>
             <Panel></Panel>
