7e5c1eca4a333d876c86444775eac40361cf2d3a
Francisco alfaro <franciscoalfar@gmail.com>
2024-05-16 01:12:47 -0400

    mejora

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -11,16 +11,17 @@ export const Forms = ({ nuevaLocalizacion }) => {
         nuevaLocalizacion(city);
     }
     return (
-        <div className="container mx-auto py-8">
-            <div className="max-w-lg mx-auto">
-                <div className="mt-4 flex justify-center items-center">
-                    <form id="searchForm" onSubmit={onSubmit}>
-                        <input type="text" id="locationInput" placeholder="Ingrese una ubicación..." className="flex-1 rounded-l-md px-4 py-2 border-t border-b border-l text-gray-800 border-gray-300 bg-white" onChange={(e) => setCity(e.target.value)}></input>
-                        <button type='submit' className="rounded-r-md px-6 py-2 bg-blue-500 text-white font-semibold hover:bg-blue-600 transition duration-200">Consultar Clima</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div className="container mx-auto py-8">
+  <div className="max-w-lg mx-auto">
+    <div className="mt-4 flex flex-col sm:flex-row justify-center items-center">
+      <form id="searchForm" onSubmit={onSubmit} className="flex-1 flex sm:mr-4">
+        <input type="text" id="locationInput" placeholder="Ingrese una ubicación..." className="flex-1 rounded-l-md px-4 py-2 border-t border-b border-l text-gray-800 border-gray-300 bg-white" onChange={(e) => setCity(e.target.value)} />
+        <button type='submit' className="rounded-r-md px-6 py-2 bg-blue-500 text-white text-sm font-semibold hover:bg-blue-600 transition duration-200">Consultar</button>
+      </form>
+
+    </div>
+  </div>
+</div>
 
 
     )
