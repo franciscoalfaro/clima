f28f73fe1bcf10639e7c55ddcf6dc187cd8b1221
Francisco alfaro <franciscoalfar@gmail.com>
2024-05-16 01:28:41 -0400

    mejora

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -18,7 +18,6 @@ export const Forms = ({ nuevaLocalizacion }) => {
                         <input type="text" id="locationInput" placeholder="Ingrese una ubicación..." className="flex-1 rounded-l-md px-4 py-2 border-t border-b border-l text-gray-800 border-gray-300 bg-white" onChange={(e) => setCity(e.target.value)} />
                         <button type='submit' className="rounded-r-md px-6 py-2 bg-blue-500 text-white text-sm font-semibold hover:bg-blue-600 transition duration-200">Consultar</button>
                     </form>
-
                 </div>
             </div>
         </div>
